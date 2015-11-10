65af51ecb473b81716998d580dd2e21815716456
Rhys Bartels-Waller <rhyslbw@gmail.com>
2015-11-10 17:42:39 +1100

    TestRunner now available on client
    Also brings in pre-release package support with mgp

--- package.js
+++ package.js
@@ -14,22 +14,27 @@ Package.onUse(function(api) {
     'underscore',
     'check',
     'mongo',
+    'space:vo-numeral@0.1.0',
     'space:base@3.2.1',
-    'space:messaging@2.1.0'
+    'space:messaging@2.1.0',
+    'space:event-sourcing@2.1.0'
   ]);
 
   api.addFiles('source/client/module.js','client');
   api.addFiles('source/server/module.js','server');
 
   api.addFiles([
-    'source/commands.js'
+    'source/value_objects/load_test_run_spec.js',
+    //'source/commands.js',
+    'source/events.js',
+    'source/load_test_runner.js'
   ]);
 
-  api.addFiles([
-    'source/server/value_objects/load_test_run_spec.js',
-    'source/server/load_test_runner.js',
-    'source/server/events.js'
-  ], 'server');
+  //api.addFiles([
+    //'source/server/events.js',
+    //'source/server/domain/load_tests/load_test.js',
+    //'source/server/domain/load_tests/load_test_router.js'
+  //], 'server');
 
 
 });
@@ -42,14 +47,19 @@ Package.onTest(function(api) {
     'ejson',
     'mongo',
     'underscore',
+    'space:vo-numeral',
     'space:performance',
     'space:messaging@2.1.0',
     'space:testing@2.0.0',
-    'practicalmeteor:munit@2.1.5',
+    'practicalmeteor:munit@2.1.5'
   ]);
 
   api.addFiles([
-    'tests/load_test_runner.tests.coffee',
-  ], 'server');
+    'tests/load_test_runner.tests.coffee'
+  ]);
+
+  //api.addFiles([
+    //'tests/server/load_test.tests.js'
+  //], 'server');
 
 });
--- source/server/module.js
+++ source/server/module.js
@@ -3,7 +3,7 @@ Space.performance = Space.Module.define('Space.performance', {
 
   RequiredModules: ['Space.messaging'],
 
-  onInitialize: function(){
+  onInitialize: function () {
     this.injector.map('Space.performance.LoadTestRunner').asStaticValue();
   }
 
